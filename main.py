5012c63e8a4f8dc7e27e4c6eb73fb0a0d854f766
Season Saw <season.saw@paywatchglobal.com>
2024-10-03 17:10:34 +0800

    add pico to header

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fasthtml.common import *
 
-app = FastHTML()
+app = FastHTML(hdrs=(picolink))
 
 @app.get("/")
 def home():
-    page = Html(
-        Head(Title("Some page")),
-        Body(Div("Hello world!")),
-    )
+    # page = Html(
+    #     Head(Title("Some page")),
+    #     Body(Main(Div("Hello world!"))),
+    # )
+    page = (Title("A Title"),
+            Main(Div("Hello world!")))
     
     return page
 
